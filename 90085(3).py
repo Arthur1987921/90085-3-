0a85467fae7e7ab125fbe95ddaf31b8f6c15ea11
Arthur1987921 <70871859+Arthur1987921@users.noreply.github.com>
2020-09-28 11:36:20 +0800

    Add files via upload

---
+++ 90085(3).py
@@ -0,0 +1,34 @@
+# score=['小徐',5,9,6,8,7,10,6]
+# print(score[0:7])
+
+# score.pop(0)
+# a=max(score)
+# print("小徐最多投進",a,'球')
+
+# b=min(score)
+# print("小徐最少投進",b,'球')
+
+# print(sorted(score)[-3:])
+
+# print(sorted(score)[:3])
+
+# a=sum(score)/len(score)
+# print(a)
+
+# import random
+# a='I love you'
+# b='so'
+# c='much'
+# e=random.randint(1,100)
+# for i in e:
+    
+    
+#     d=(b+' ')*i
+#     print(f'{a} {d}{c}')
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         k=i*j
+#         print('%dx%d=%2d' % (i, j, k), end=' ')
+#     print()
+ 
